20217246614c407727d464bf55c939f94b034cae
berlin-goen <berlin.pham@creatorchain.network>
2022-01-17 20:20:25 +0700

    Approve for wbnb

--- approve.js
+++ approve.js
@@ -8,11 +8,13 @@ const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
 const BUSD = new ethers.Contract(process.env.BUSD_ADDRESS, abi.BEP20, wallet)
 const MXY = new ethers.Contract(process.env.MXY_ADDRESS, abi.BEP20, wallet)
+const WBNB = new ethers.Contract(process.env.WBNB_ADDRESS, abi.BEP20, wallet)
 
 
 const main = async () => {
     await BUSD.approve(process.env.PANCAKE_ROUTER_ADDRESS, '100000000000000000000000000000')
     await MXY.approve(process.env.PANCAKE_ROUTER_ADDRESS, '100000000000000000000000000000')
+    await WBNB.approve(process.env.PANCAKE_ROUTER_ADDRESS, '100000000000000000000000000000')
     console.log('approve finished')
 }
 
