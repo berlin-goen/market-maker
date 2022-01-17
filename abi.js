ecded1c742051e523062c5f0d1ef8dd44142a4d1
berlin-goen <berlin.pham@creatorchain.network>
2022-01-17 19:39:21 +0700

    Approve for pancake

--- abi.js
+++ abi.js
@@ -18,6 +18,30 @@ module.exports = {
             ],
             "stateMutability": "view",
             "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
         }
     ],
     PANCAKE_ROUTER: [
---
+++ approve.js
@@ -0,0 +1,21 @@
+const ethers = require('ethers')
+require('dotenv').config()
+
+const abi = require('./abi')
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_RPC)
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+const BUSD = new ethers.Contract(process.env.BUSD_ADDRESS, abi.BEP20, wallet)
+const MXY = new ethers.Contract(process.env.MXY_ADDRESS, abi.BEP20, wallet)
+
+
+const main = async () => {
+    await BUSD.approve(process.env.PANCAKE_ROUTER_ADDRESS, '100000000000000000000000000000')
+    await MXY.approve(process.env.PANCAKE_ROUTER_ADDRESS, '100000000000000000000000000000')
+    console.log('approve finished')
+}
+
+main()
+.then(console.log)
+.catch(console.log)
